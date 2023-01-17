c8513aac11463249dffcd6cc37cc00378080ff98
Stevie <mail@steviegoodman.com>
2023-01-17 12:51:54 +0000

    Abilities now have holding requirement

--- Assets/Scripts/Player/HackAbility.cs
+++ Assets/Scripts/Player/HackAbility.cs
@@ -1,9 +1,12 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Interactions;
 using UnityEngine.Serialization;
 
 public class HackAbility : Ability
 {
+    private InputAction _actionAsset;
     [SerializeField] private LayerMask layerMask;
     
     public void Awake()
@@ -11,10 +14,29 @@ public class HackAbility : Ability
         PlayerPrefs.SetInt("HackReputation", 5);
         Reputation = PlayerPrefs.GetInt("HackReputation");
     }
-        
+    
+    private void Start()
+    {
+        _actionAsset = GetComponent<PlayerInput>().actions["Player/Hack"];
+        OnReputationChange();
+        reputationValueAltered.AddListener(OnReputationChange);
+    }
+
+    private void OnReputationChange()
+    {
+        float holdDuration = AbilityLevel switch
+        {
+            AbilityLevel.Positive => 1,
+            AbilityLevel.Neutral => 3,
+            AbilityLevel.Negative => 5,
+            _ => throw new ArgumentOutOfRangeException()
+        };
+        GetComponent<PlayerInput>().actions["Player/Hack"].ApplyParameterOverride((HoldInteraction interaction) => interaction.duration, holdDuration);
+    }
+    
     public override void UseAbility(InputAction.CallbackContext context)
     {
-        if (!context.started) return;
+        if (!context.performed) return;
         if (!HitComponent<Hackable>(out RaycastHit2D raycastHit, layerMask)) return;
         if (Charges <= 0) return;
         Charges--;
